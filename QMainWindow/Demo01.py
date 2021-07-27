60070747177f9ba404f9e413326ce9bb658749f6
= <=>
2021-07-27 16:49:39 +0800

    修改了点东西

--- QMainWindow/Demo01.py
+++ QMainWindow/Demo01.py
@@ -4,8 +4,8 @@
 """
 QMainWindow包括四个部分：
     1. Menu Bar ： 位于MainWindow最上面
-    2. ToolBars :  位于Center Wideget外层 
-    3. Center Wideget： 只有一个，位于最中间
+    2. ToolBars :  位于Center Widget外层
+    3. Center Widget： 只有一个，位于最中间
     4. status bar ：位于最下方，通常显示一些状态信息
 """ 
 import sys
@@ -18,8 +18,17 @@ class MainWind(QMainWindow):
     def __init__(self):
         super(MainWind, self).__init__()
         self.setWindowTitle("title")
-        self.addToolBar()
-
-
+        self.setGeometry(500,500,500,500)
+        self.create_btn = QPushButton("CREATE")
+        # self.addToolBa的的.create_btn)
+        self.status_bar = QStatusBar(parent=self)
+        self.status_bar.showMessage("this is a status bar", 5000)
+        self.status_label = QLabel("Status Label")
+        self.status_bar.addWidget(self.status_label)
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = MainWind()
+    main.show()
+
+    sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # About
 This just is a test project for PyQt5~
 # LEARN CONTNETS
-## QMainWindw
+## GUI窗体介绍
+### QMainWindw
+QMainWindow一共有四个窗口：
+* 
+### 弹出式对话框
 
 ## QLabel
